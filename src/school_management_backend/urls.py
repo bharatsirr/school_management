553a6aa0ae8f07d90f37f0732fa0eb585b9bec37
Bharat Upadhyay <bpositiveu@gmail.com>
2025-03-25 00:46:12 +0530

    add custom login signup logout forms and configure custom auth bakend

--- src/school_management_backend/urls.py
+++ src/school_management_backend/urls.py
@@ -20,4 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path(settings.ADMIN_URL, admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', include('apps.core.urls')),
 ]
