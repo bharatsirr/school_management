e47ed268cdf27babee3db8b571763f7c13687248
Bharat Upadhyay <bpositiveu@gmail.com>
2025-03-24 16:53:44 +0530

    add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: [
+        './src/templates/**/*.{html,js}', // Scan all HTML and JS files in your project
+    ],
+    theme: {
+        extend: {
+            colors: {
+                customBlue: '#1e40af',
+            },
+        },
+    },
+};
