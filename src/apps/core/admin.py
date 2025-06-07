1c1004e2cd40c7538e6175b68d3933482fd59542
Bharat Upadhyay <bpositiveu@gmail.com>
2025-06-07 12:43:30 +0530

    update admin of core app to display user gender

--- src/apps/core/admin.py
+++ src/apps/core/admin.py
@@ -4,9 +4,9 @@ from apps.core.models import User, Phone, UserDocument, Family, FamilyMember
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'is_active', 'aadhar_number', 'is_staff', 'is_superuser', 'religion', 'caste', 'category', 'created_at', 'last_login')
-    search_fields = ('username', 'email', 'aadhar_number', 'religion', 'caste', 'category')
-    list_filter = ('is_active', 'is_staff', 'is_superuser', 'religion', 'caste', 'category')
+    list_display = ('username', 'email', 'is_active', 'aadhar_number', 'is_staff', 'is_superuser', 'gender', 'religion', 'caste', 'category', 'created_at', 'last_login')
+    search_fields = ('username', 'email', 'aadhar_number', 'religion', 'caste', 'category', 'gender')
+    list_filter = ('is_active', 'is_staff', 'is_superuser', 'religion', 'caste', 'category', 'gender')
 
 @admin.register(Phone)
 class PhoneAdmin(admin.ModelAdmin):
